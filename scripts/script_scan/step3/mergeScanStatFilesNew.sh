3a328b2aff394ff42886e8fd6591f5af1c7e8cbe
jhoss <hoss@phys.ethz.ch>
2016-07-22 12:18:50 +0200

    SUSY3L: prepare scan scripts for full data

--- scripts/script_scan/step3/mergeScanStatFilesNew.sh
+++ scripts/script_scan/step3/mergeScanStatFilesNew.sh
@@ -17,7 +17,7 @@ for i in `ls $MPAF/workdir/stats/SUSY3L/3l_scanSig_*.dat`; do
 done
 
 ### merge root files
-files="vector<string> tks; tks.push_back(\"$MPAF/workdir/root/SUSY3L/merged_v2_4fb_Bkg.root\");"
+files="vector<string> tks; tks.push_back(\"$MPAF/workdir/root/SUSY3L/merged_v2_12fb_Bkg.root\");"
 for i in `ls $MPAF/workdir/root/SUSY3L/3l_scanSig_*.root`; do
     files=$files" tks.push_back(\"$i\");"
 done
