d663e00d41c3d2274f07a2ffe7fc774cc47ac1bf
jhoss <hoss@phys.ethz.ch>
2016-07-28 08:36:44 +0200

    SUSY3L: add fs lepton SF uncert to datacard

--- display/cards/susy3lScanCombine.C
+++ display/cards/susy3lScanCombine.C
@@ -5,14 +5,14 @@ void susy3l_scanSigBENCH(){
 
     //general parameters ********************* general parameters
     //string sig="fs_t1ttttBENCH";
-    string sig="fs_t1ttttBENCH";
+    string sig="fs_t5qqqqvvBENCH";
     bool allSR = true;
     bool aggregate = false;
 
     string dir="SUSY3L";
     string fileName="merged_12fb";
     if(sig=="fs_t1ttttBENCH") string fileList="merged_12_T1tttt_MASS"; //susy3lUnc
-    if(sig=="fs_t5qqqqvvBENCH") string fileList="merged_v2_12fb_T5qqqqVV_noDM_MASS"; //susy3lUnc
+    if(sig=="fs_t5qqqqvvBENCH") string fileList="merged_12_T5qqqqVV_noDM_MASS"; //susy3lUnc
     if(sig=="fs_t6ttww_50BENCH") string fileList="merged_2fb_T6ttWWMASS"; //susy3lUnc
     if(sig=="fs_t5tttt_degenBENCH") string fileList="merged_2fb_T5ttttMASS"; //susy3lUnc
   
@@ -151,7 +151,7 @@ void susy3l_scanSigBENCH(){
 
     //fastSim related uncertainties
     md.addNuisanceParameter("fs_btag"           ,sig                                    ,"shape","");
-    //md.addNuisanceParameter("fs_lep"            ,sig                                    ,"shape","");
+    md.addNuisanceParameter("fs_lep"            ,sig                                    ,"shape","");
     md.addNuisanceParameter("isr"               ,sig                                    ,"shape","");
 	md.addNuisanceParameter("scale"             ,sig                                    ,"shape","");
 	md.addNuisanceParameter("met_fast"          ,sig                                    ,"shape","");
