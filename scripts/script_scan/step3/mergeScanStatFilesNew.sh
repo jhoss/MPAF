ac2c6c67db3ddfb992287daf9160cfb06a49c46c
jhoss <hoss@phys.ethz.ch>
2016-08-03 10:21:12 +0200

    SUSY3L: minor script changes

--- scripts/script_scan/step3/mergeScanStatFilesNew.sh
+++ scripts/script_scan/step3/mergeScanStatFilesNew.sh
@@ -8,23 +8,23 @@ mainFile=${MPAF}/workdir/stats/${AN}/${folder}/merged_12fb_Bkg.dat
 ## first merge backgrounds
 python ${MPAF}/scripts/script_scan/step3/mergeBkgs.py ${mainFile} ${MPAF}/workdir/stats/${AN}/${folder}/3l_v2_scanBkg_
 
-## merge signals and backgrounds
-for i in `ls $MPAF/workdir/stats/${AN}/${folder}/3l_scanSig_*.dat`; do
-
-    red=${i#$MPAF/workdir/stats/${AN}/${folder}/3l_scanSig_}
-    echo $red
-    python ${MPAF}/scripts/script_scan/step3/addSample.py $mainFile $i $MPAF/workdir/stats/${AN}/${folder}/merged_12_$red
-
-done
-
-### merge root files
-files="vector<string> tks; tks.push_back(\"$MPAF/workdir/root/${AN}/${folder}/merged_v2_12fb_Bkg.root\");"
-for i in `ls $MPAF/workdir/root/${AN}/${folder}/3l_scanSig_*.root`; do
-    files=$files" tks.push_back(\"$i\");"
-done
-
-#past string to next step
-echo $files
+### merge signals and backgrounds
+#for i in `ls $MPAF/workdir/stats/${AN}/${folder}/3l_scanSig_*.dat`; do
+#
+#    red=${i#$MPAF/workdir/stats/${AN}/${folder}/3l_scanSig_}
+#    echo $red
+#    python ${MPAF}/scripts/script_scan/step3/addSample.py $mainFile $i $MPAF/workdir/stats/${AN}/${folder}/merged_12_$red
+#
+#done
+#
+#### merge root files
+#files="vector<string> tks; tks.push_back(\"$MPAF/workdir/root/${AN}/${folder}/merged_v2_12fb_Bkg.root\");"
+#for i in `ls $MPAF/workdir/root/${AN}/${folder}/3l_scanSig_*.root`; do
+#    files=$files" tks.push_back(\"$i\");"
+#done
+#
+##past string to next step
+#echo $files
 
 
 
